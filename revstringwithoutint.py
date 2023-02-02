7c6235415e2f0967fe9444ece29be0a6d4c0c8fe
Savan <savangawade@gmail.com>
2023-02-02 22:19:48 +0530

    Python

--- revstringwithoutint.py
+++ revstringwithoutint.py
@@ -1,4 +1,4 @@
-a="sa93va45n"
+a="sa93va45na"
 lists=list(a)
 i=0
 j=len(lists)-1
@@ -13,3 +13,19 @@ while i<j:
         j-=1
 rev=''.join(lists)
 print(f"reveese string without number reverse:",rev)
+# def reverse_string(a):
+#     temp=[]
+#     a=list(a)
+#     for i in a:
+#         if not i.isnumeric():
+#             temp.append(i)
+#     reverse_temp=temp[::-1]
+#     count=0
+#     for i in range(0,len(a)):
+#         if not a[i].isnumeric():
+#             a[i]=reverse_temp[count]
+#             count+=1
+#         else:
+#             continue
+#         return"".join(a)
+# print(reverse_string(a))
