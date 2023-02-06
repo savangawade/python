dcfc670d89c67fde87a480c31d8028285adf8c8c
Savan <savangawade@gmail.com>
2023-02-06 23:42:44 +0530

    python

--- factorial.py
+++ factorial.py
@@ -6,9 +6,18 @@
 
 
 
+# n=int(input("enter the number="))
+# fact=1
+# if n>0:
+#     for i in range(1,n+1):
+#         fact=fact*i
+# print("factorial of",n,"is=",fact)
+
+
 n=int(input("enter the number="))
+count=2
 fact=1
-if n>0:
-    for i in range(1,n+1):
-        fact=fact*i
+while (count<=n):
+    fact=fact*count
+    count=count+1
 print("factorial of",n,"is=",fact)
