e802eefe36628c868de3adea67af3fdc7dfee38c
Savan <savangawade@gmail.com>
2023-02-06 16:29:14 +0530

    python

--- numberpattern.py
+++ numberpattern.py
@@ -83,24 +83,32 @@
 #     print()
 
 
-n=int(input("enter number="))
-x=int(input("enter another number="))    # x should be 2n
-for i in range(n):
-    for j in range(n-i-1):
-        print(" ",end='')
-    for j in range(i+1):
-        print('* ',end='')
-    for j in range(2*(n-i-1)):
-        print(" ",end='')
-    for j in range(i+1):
-        print('* ',end='')
-    print()
-#x=2*n
-for i in range(x,0,-1):
-    for j in range(x-i):
-        print(' ',end='')
-    for j in range(i,0,-1):
-        print('* ',end='')
+# n=int(input("enter number="))
+# x=int(input("enter another number="))    # x should be 2n
+# for i in range(n):
+#     for j in range(n-i-1):
+#         print(" ",end='')
+#     for j in range(i+1):
+#         print('* ',end='')
+#     for j in range(2*(n-i-1)):
+#         print(" ",end='')
+#     for j in range(i+1):
+#         print('* ',end='')
+#     print()
+# #x=2*n
+# for i in range(x,0,-1):
+#     for j in range(x-i):
+#         print(' ',end='')
+#     for j in range(i,0,-1):
+#         print('* ',end='')
+#     print()
+
+for row in range(6):
+    for col in range(7):
+        if(row==0 and col%3!=0) or (row==1 and col%3==0) or (row-col==2) or (row+col==8):
+            print('*',end=' ')
+        else:
+            print(" ",end=' ')
     print()
         
 
