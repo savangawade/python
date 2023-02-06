902a4c1f5fb1804e5ab21641d22ab0c65f586f74
Savan <savangawade@gmail.com>
2023-02-06 23:42:22 +0530

    python

---
+++ constructor.py
@@ -0,0 +1,13 @@
+class Addition:
+    def __init__(self,a,b,c):
+        self.a=a
+        self.b=b
+        self.c=c
+    def sum(self):
+        z=self.a+self.b+self.c
+        print("sum is:",z)
+a=int(input("enter first number:"))
+b=int(input("enter second number:"))
+c=int(input("enter third number:"))
+obj=Addition(a,b,c)
+obj.sum()
