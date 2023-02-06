6adce13cd791d9c290bb9d060d6c9d19da7433db
Savan <savangawade@gmail.com>
2023-02-06 15:02:05 +0530

    python

--- star.py
+++ star.py
@@ -11,55 +11,57 @@
 #         print('*',end='')
 #     print()
 ###############################################################
-# n=5
+n=3
+for i in range(n):
+    for j in range(i):
+        print('*',end=' ')
+    for j in range(i,n):
+        print(' ',end=' ')
+    for j in range(i+n-2):
+        print('*',end=' ')
+    print()
+for i in range(n):
+    for j in range(n-i):
+        print('*',end=' ')
+    print()
+
+# n=3
 # for i in range(n):
 #     for j in range(i+1):
-#         print('*',end='')
+#         print('',end=' ')
+#     for j in range(i):
+#         print('*',end=' ')
+#     for j in range(i,n-1):
+#         print('*',end=' ')
 #     print()
 
-#     n=5
+# n=3
 # for i in range(n):
-#     for j in range(i,n):
-#         print('*',end='')
-#     print()
-
-# n=5
-# for i in range(n):
+#     for j in range(i,n-1):
+#         print('',end=' ')
+#     for j in range(i,):
+#         print('*',end=' ')
 #     for j in range(i+1):
-#         print(' ',end='')
+#         print('*',end=' ')
+#     print()
+#Diamond
+# n=3
+# for i in range(n-1):
+#     for j in range(n-i):
+#         print(' ',end=' ')
 #     for j in range(i):
 #         print('*',end=' ')
 #     for j in range(i+1):
 #         print('*',end=' ')
 #     print()
-
-# n=5
 # for i in range(n):
-#     for j in range(i,n):
-#         print('',end='')
-#     for j in range(i,n-1):
-#         print('*',end='')
-#     for j in range(i,n):
-#         print('*',end='')
-#     print()
-
-n=5
-for i in range(n-1):
-    for j in range(n-i):
-        print(' ',end='')
-    for j in range(i):
-        print('*',end='')
-    for j in range(i+1):
-        print('*',end='')
-    print()
-for i in range(n):
-        for j in range(i+1):
-         print(' ',end='')
-        for j in range(i,n-1):
-         print('*',end='')
-        for j in range(i,n):
-         print('*',end='')
-        print()
+#         for j in range(i+1):
+#          print(' ',end=' ')
+#         for j in range(i,n-1):
+#          print('*',end=' ')
+#         for j in range(i,n):
+#          print('*',end=' ')
+#         print()
 
 # n=5
 # for i in range(n):
---
+++ trial.py
@@ -0,0 +1,21 @@
+n=3
+for i in range(n):
+    for j in range(i+1):
+        print('*',end=' ')
+    for j in range(i,n-1):
+         print(' ',end=' ')
+    for j in range(i,n-1):
+        print(' ',end=' ')
+    for j in range(i+1):
+        print('*',end=' ')
+    print()
+for i in range(n):
+    for j in range(n-i-1):
+        print('*',end=' ')
+    for j in range(i):
+        print(' ',end=' ')
+    for j in range(i+n-1):
+        print(' ',end=' ')
+    for j in range(i,n-1):
+        print('*',end=' ')
+    print()
